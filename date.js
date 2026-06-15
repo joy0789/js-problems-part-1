fa0a3fedf987ee099aacae3fceecc1edaa6d59cc
joy0789 <johirulislamj62@gmail.comm>
2026-06-15 18:13:20 +0600

    A Simple introduction to JS Date and time

---
+++ date.js
@@ -0,0 +1,10 @@
+let today = new Date();
+let date = new Date('2026-10-19')
+console.log(date.getMonth());
+console.log(date.getDay());
+let specificDate = new Date(2091, 0 ,26)
+console.log(specificDate);
+specificDate.setMonth(7);
+console.log(specificDate.toLocaleString('en-GB'));
+
+
