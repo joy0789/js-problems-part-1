3937a38208d65b75ce6331c595a3915e3eed64e8
joy0789 <johirulislamj62@gmail.comm>
2026-06-15 18:01:59 +0600

    Math, abs, round, ceil, floor, and random number

---
+++ math.js
@@ -0,0 +1,18 @@
+// let min = Math.min(234,5,84,39,7,28,385,2,596);
+// console.log(min)
+// let max = Math.min(234,5,84,39,7,28,385,2,596);
+// console.log(max)
+
+// console.log(Math.PI)
+
+// console.log(Math.PI)
+
+
+// console.log(Math.round(2.255))
+// console.log(Math.round(2.85))
+// console.log(Math.floor(2.85))
+// console.log(Math.ceil(2.85))
+// console.log(Math.ceil(2.0001))
+
+console.log(Math.random())
+console.log(Math.random()*10)
